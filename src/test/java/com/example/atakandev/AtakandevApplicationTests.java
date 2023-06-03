ded5792b104156ca1c47e588b47d8d2e8fd83bf1
atakanoguz <atakan17171717@hotmail.com>
2023-06-03 20:12:22 +0300

    Initial commit

---
+++ src/test/java/com/example/atakandev/AtakandevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atakandev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtakandevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
